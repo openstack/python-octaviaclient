openstack/python-octaviaclient

commit ae244fcf25188f38c28870238e648e765a2e3adf

Add "--wait" functionality
Change-Id: Ia8e451d7f806fac7d36b2860f256eb34f9f7400e

octaviaclient/api/v2/octavia.py
@@ -18,6 +18,7 @@ from osc_lib.api import api
 from osc_lib import exceptions as osc_exc
 
 from octaviaclient.api import constants as const
+from octaviaclient.api import exceptions
 
 
 def correct_return_codes(func):
@@ -45,7 +46,7 @@ def correct_return_codes(func):
             else:
                 raise
 
-            raise OctaviaClientException(
+            raise exceptions.OctaviaClientException(
                 code=code,
                 message=message,
                 request_id=request_id)
@@ -934,17 +935,3 @@ class OctaviaAPI(api.BaseAPI):
         response = self._delete(url)
 
         return response
-
-
-class OctaviaClientException(Exception):
-    """The base exception class for all exceptions this library raises."""
-
-    def __init__(self, code, message=None, request_id=None):
-        self.code = code
-        self.message = message or self.__class__.message
-        self.request_id = request_id
-
-    def __str__(self):
-        return "%s (HTTP %s) (Request-ID: %s)" % (self.message,
-                                                  self.code,
-                                                  self.request_id)

octaviaclient/tests/unit/api/test_octavia.py
@@ -19,6 +19,7 @@ from requests_mock.contrib import fixture
 
 from osc_lib.tests import utils
 
+from octaviaclient.api import exceptions
 from octaviaclient.api.v2 import octavia
 
 FAKE_ACCOUNT = 'q12we34r'
@@ -204,7 +205,7 @@ class TestLoadBalancer(TestOctaviaClient):
             text='{"faultstring": "%s"}' % self._error_message,
             status_code=400
         )
-        self.assertRaisesRegex(octavia.OctaviaClientException,
+        self.assertRaisesRegex(exceptions.OctaviaClientException,
                                self._error_message,
                                self.api.load_balancer_create,
                                json=SINGLE_LB_RESP)
@@ -226,7 +227,7 @@ class TestLoadBalancer(TestOctaviaClient):
             text='{"faultstring": "%s"}' % self._error_message,
             status_code=400
         )
-        self.assertRaisesRegex(octavia.OctaviaClientException,
+        self.assertRaisesRegex(exceptions.OctaviaClientException,
                                self._error_message,
                                self.api.load_balancer_set,
                                FAKE_LB,
@@ -248,7 +249,7 @@ class TestLoadBalancer(TestOctaviaClient):
             text='{"faultstring": "%s"}' % self._error_message,
             status_code=409,
         )
-        self.assertRaisesRegex(octavia.OctaviaClientException,
+        self.assertRaisesRegex(exceptions.OctaviaClientException,
                                self._error_message,
                                self.api.load_balancer_failover,
                                FAKE_LB)
@@ -269,7 +270,7 @@ class TestLoadBalancer(TestOctaviaClient):
             text='{"faultstring": "%s"}' % self._error_message,
             status_code=400
         )
-        self.assertRaisesRegex(octavia.OctaviaClientException,
+        self.assertRaisesRegex(exceptions.OctaviaClientException,
                                self._error_message,
                                self.api.load_balancer_delete,
                                FAKE_LB)
@@ -331,7 +332,7 @@ class TestLoadBalancer(TestOctaviaClient):
             text='{"faultstring": "%s"}' % self._error_message,
             status_code=400
         )
-        self.assertRaisesRegex(octavia.OctaviaClientException,
+        self.assertRaisesRegex(exceptions.OctaviaClientException,
                                self._error_message,
                                self.api.listener_create,
                                json=SINGLE_LI_RESP)
@@ -353,7 +354,7 @@ class TestLoadBalancer(TestOctaviaClient):
             text='{"faultstring": "%s"}' % self._error_message,
             status_code=400
         )
-        self.assertRaisesRegex(octavia.OctaviaClientException,
+        self.assertRaisesRegex(exceptions.OctaviaClientException,
                                self._error_message,
                                self.api.listener_set,
                                FAKE_LI, json=SINGLE_LI_UPDATE)
@@ -374,7 +375,7 @@ class TestLoadBalancer(TestOctaviaClient):
             text='{"faultstring": "%s"}' % self._error_message,
             status_code=400
         )
-        self.assertRaisesRegex(octavia.OctaviaClientException,
+        self.assertRaisesRegex(exceptions.OctaviaClientException,
                                self._error_message,
                                self.api.listener_delete,
                                FAKE_LI)
@@ -426,7 +427,7 @@ class TestLoadBalancer(TestOctaviaClient):
             text='{"faultstring": "%s"}' % self._error_message,
             status_code=400
         )
-        self.assertRaisesRegex(octavia.OctaviaClientException,
+        self.assertRaisesRegex(exceptions.OctaviaClientException,
                                self._error_message,
                                self.api.pool_create,
                                json=SINGLE_PO_RESP)
@@ -448,7 +449,7 @@ class TestLoadBalancer(TestOctaviaClient):
             text='{"faultstring": "%s"}' % self._error_message,
             status_code=400
         )
-        self.assertRaisesRegex(octavia.OctaviaClientException,
+        self.assertRaisesRegex(exceptions.OctaviaClientException,
                                self._error_message,
                                self.api.pool_set,
                                FAKE_PO, json=SINGLE_PO_UPDATE)
@@ -469,7 +470,7 @@ class TestLoadBalancer(TestOctaviaClient):
             text='{"faultstring": "%s"}' % self._error_message,
             status_code=400
         )
-        self.assertRaisesRegex(octavia.OctaviaClientException,
+        self.assertRaisesRegex(exceptions.OctaviaClientException,
                                self._error_message,
                                self.api.pool_delete,
                                FAKE_PO)
@@ -511,7 +512,7 @@ class TestLoadBalancer(TestOctaviaClient):
             text='{"faultstring": "%s"}' % self._error_message,
             status_code=400
         )
-        self.assertRaisesRegex(octavia.OctaviaClientException,
+        self.assertRaisesRegex(exceptions.OctaviaClientException,
                                self._error_message,
                                self.api.member_create,
                                json=SINGLE_ME_RESP, pool_id=FAKE_PO)
@@ -534,7 +535,7 @@ class TestLoadBalancer(TestOctaviaClient):
             text='{"faultstring": "%s"}' % self._error_message,
             status_code=400
         )
-        self.assertRaisesRegex(octavia.OctaviaClientException,
+        self.assertRaisesRegex(exceptions.OctaviaClientException,
                                self._error_message,
                                self.api.member_set,
                                pool_id=FAKE_PO, member_id=FAKE_ME,
@@ -556,7 +557,7 @@ class TestLoadBalancer(TestOctaviaClient):
             text='{"faultstring": "%s"}' % self._error_message,
             status_code=400
         )
-        self.assertRaisesRegex(octavia.OctaviaClientException,
+        self.assertRaisesRegex(exceptions.OctaviaClientException,
                                self._error_message,
                                self.api.member_delete,
                                pool_id=FAKE_PO, member_id=FAKE_ME)
@@ -598,7 +599,7 @@ class TestLoadBalancer(TestOctaviaClient):
             text='{"faultstring": "%s"}' % self._error_message,
             status_code=400
         )
-        self.assertRaisesRegex(octavia.OctaviaClientException,
+        self.assertRaisesRegex(exceptions.OctaviaClientException,
                                self._error_message,
                                self.api.l7policy_create,
                                json=SINGLE_L7PO_RESP)
@@ -620,7 +621,7 @@ class TestLoadBalancer(TestOctaviaClient):
             text='{"faultstring": "%s"}' % self._error_message,
             status_code=400
         )
-        self.assertRaisesRegex(octavia.OctaviaClientException,
+        self.assertRaisesRegex(exceptions.OctaviaClientException,
                                self._error_message,
                                self.api.l7policy_set,
                                FAKE_L7PO, json=SINGLE_L7PO_UPDATE)
@@ -641,7 +642,7 @@ class TestLoadBalancer(TestOctaviaClient):
             text='{"faultstring": "%s"}' % self._error_message,
             status_code=400
         )
-        self.assertRaisesRegex(octavia.OctaviaClientException,
+        self.assertRaisesRegex(exceptions.OctaviaClientException,
                                self._error_message,
                                self.api.l7policy_delete,
                                FAKE_L7PO)
@@ -683,7 +684,7 @@ class TestLoadBalancer(TestOctaviaClient):
             text='{"faultstring": "%s"}' % self._error_message,
             status_code=400
         )
-        self.assertRaisesRegex(octavia.OctaviaClientException,
+        self.assertRaisesRegex(exceptions.OctaviaClientException,
                                self._error_message,
                                self.api.l7rule_create,
                                FAKE_L7PO, json=SINGLE_L7RU_RESP)
@@ -709,7 +710,7 @@ class TestLoadBalancer(TestOctaviaClient):
             text='{"faultstring": "%s"}' % self._error_message,
             status_code=400
         )
-        self.assertRaisesRegex(octavia.OctaviaClientException,
+        self.assertRaisesRegex(exceptions.OctaviaClientException,
                                self._error_message,
                                self.api.l7rule_set,
                                l7rule_id=FAKE_L7RU,
@@ -735,7 +736,7 @@ class TestLoadBalancer(TestOctaviaClient):
             text='{"faultstring": "%s"}' % self._error_message,
             status_code=400
         )
-        self.assertRaisesRegex(octavia.OctaviaClientException,
+        self.assertRaisesRegex(exceptions.OctaviaClientException,
                                self._error_message,
                                self.api.l7rule_delete,
                                l7rule_id=FAKE_L7RU,
@@ -778,7 +779,7 @@ class TestLoadBalancer(TestOctaviaClient):
             text='{"faultstring": "%s"}' % self._error_message,
             status_code=400
         )
-        self.assertRaisesRegex(octavia.OctaviaClientException,
+        self.assertRaisesRegex(exceptions.OctaviaClientException,
                                self._error_message,
                                self.api.health_monitor_create,
                                json=SINGLE_HM_RESP)
@@ -800,7 +801,7 @@ class TestLoadBalancer(TestOctaviaClient):
             text='{"faultstring": "%s"}' % self._error_message,
             status_code=400
         )
-        self.assertRaisesRegex(octavia.OctaviaClientException,
+        self.assertRaisesRegex(exceptions.OctaviaClientException,
                                self._error_message,
                                self.api.health_monitor_set,
                                FAKE_HM, json=SINGLE_HM_UPDATE)
@@ -821,7 +822,7 @@ class TestLoadBalancer(TestOctaviaClient):
             text='{"faultstring": "%s"}' % self._error_message,
             status_code=400
         )
-        self.assertRaisesRegex(octavia.OctaviaClientException,
+        self.assertRaisesRegex(exceptions.OctaviaClientException,
                                self._error_message,
                                self.api.health_monitor_delete,
                                FAKE_HM)
@@ -863,7 +864,7 @@ class TestLoadBalancer(TestOctaviaClient):
             text='{"faultstring": "%s"}' % self._error_message,
             status_code=400
         )
-        self.assertRaisesRegex(octavia.OctaviaClientException,
+        self.assertRaisesRegex(exceptions.OctaviaClientException,
                                self._error_message,
                                self.api.quota_set,
                                FAKE_PRJ, json=SINGLE_QT_UPDATE)
@@ -884,7 +885,7 @@ class TestLoadBalancer(TestOctaviaClient):
             text='{"faultstring": "%s"}' % self._error_message,
             status_code=400
         )
-        self.assertRaisesRegex(octavia.OctaviaClientException,
+        self.assertRaisesRegex(exceptions.OctaviaClientException,
                                self._error_message,
                                self.api.quota_reset,
                                FAKE_PRJ)
@@ -925,7 +926,7 @@ class TestLoadBalancer(TestOctaviaClient):
             text='{"faultstring": "%s"}' % self._error_message,
             status_code=409,
         )
-        self.assertRaisesRegex(octavia.OctaviaClientException,
+        self.assertRaisesRegex(exceptions.OctaviaClientException,
                                self._error_message,
                                self.api.amphora_configure,
                                FAKE_AMP)
@@ -946,7 +947,7 @@ class TestLoadBalancer(TestOctaviaClient):
             text='{"faultstring": "%s"}' % self._error_message,
             status_code=409,
         )
-        self.assertRaisesRegex(octavia.OctaviaClientException,
+        self.assertRaisesRegex(exceptions.OctaviaClientException,
                                self._error_message,
                                self.api.amphora_failover,
                                FAKE_AMP)
@@ -1010,7 +1011,7 @@ class TestLoadBalancer(TestOctaviaClient):
             text='{"faultstring": "%s"}' % self._error_message,
             status_code=400
         )
-        self.assertRaisesRegex(octavia.OctaviaClientException,
+        self.assertRaisesRegex(exceptions.OctaviaClientException,
                                self._error_message,
                                self.api.flavor_create,
                                json=SINGLE_FV_RESP)
@@ -1032,7 +1033,7 @@ class TestLoadBalancer(TestOctaviaClient):
             text='{"faultstring": "%s"}' % self._error_message,
             status_code=400
         )
-        self.assertRaisesRegex(octavia.OctaviaClientException,
+        self.assertRaisesRegex(exceptions.OctaviaClientException,
                                self._error_message,
                                self.api.flavor_set,
                                FAKE_FV,
@@ -1054,7 +1055,7 @@ class TestLoadBalancer(TestOctaviaClient):
             text='{"faultstring": "%s"}' % self._error_message,
             status_code=400
         )
-        self.assertRaisesRegex(octavia.OctaviaClientException,
+        self.assertRaisesRegex(exceptions.OctaviaClientException,
                                self._error_message,
                                self.api.flavor_delete,
                                FAKE_FV)
@@ -1096,7 +1097,7 @@ class TestLoadBalancer(TestOctaviaClient):
             text='{"faultstring": "%s"}' % self._error_message,
             status_code=400
         )
-        self.assertRaisesRegex(octavia.OctaviaClientException,
+        self.assertRaisesRegex(exceptions.OctaviaClientException,
                                self._error_message,
                                self.api.flavorprofile_create,
                                json=SINGLE_FVPF_RESP)
@@ -1118,7 +1119,7 @@ class TestLoadBalancer(TestOctaviaClient):
             text='{"faultstring": "%s"}' % self._error_message,
             status_code=400
         )
-        self.assertRaisesRegex(octavia.OctaviaClientException,
+        self.assertRaisesRegex(exceptions.OctaviaClientException,
                                self._error_message,
                                self.api.flavorprofile_set,
                                FAKE_FVPF, json=SINGLE_FVPF_UPDATE)
@@ -1139,7 +1140,7 @@ class TestLoadBalancer(TestOctaviaClient):
             text='{"faultstring": "%s"}' % self._error_message,
             status_code=400
         )
-        self.assertRaisesRegex(octavia.OctaviaClientException,
+        self.assertRaisesRegex(exceptions.OctaviaClientException,
                                self._error_message,
                                self.api.flavorprofile_delete,
                                FAKE_FVPF)

octaviaclient/api/exceptions.py
@@ -1,0 +1,26 @@
+#   Licensed under the Apache License, Version 2.0 (the "License"); you may
+#   not use this file except in compliance with the License. You may obtain
+#   a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#   WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#   License for the specific language governing permissions and limitations
+#   under the License.
+#
+
+
+class OctaviaClientException(Exception):
+    """The base exception class for all exceptions this library raises."""
+
+    def __init__(self, code, message=None, request_id=None):
+        self.code = code
+        self.message = message or self.__class__.message
+        self.request_id = request_id
+
+    def __str__(self):
+        return "%s (HTTP %s) (Request-ID: %s)" % (self.message,
+                                                  self.code,
+                                                  self.request_id)

octaviaclient/osc/v2/amphora.py
@@ -128,15 +128,32 @@ class ConfigureAmphora(command.Command):
             metavar='<amphora-id>',
             help='UUID of the amphora to configure.',
         )
+        parser.add_argument(
+            '--wait',
+            action='store_true',
+            help='Wait for action to complete',
+        )
 
         return parser
 
     def take_action(self, parsed_args):
         attrs = v2_utils.get_amphora_attrs(self.app.client_manager,
                                            parsed_args)
-
+        amp_id = attrs.pop('amphora_id')
         self.app.client_manager.load_balancer.amphora_configure(
-            amphora_id=attrs.pop('amphora_id'))
+            amphora_id=amp_id)
+
+        if parsed_args.wait:
+            amphora = self.app.client_manager.load_balancer.amphora_show(
+                amp_id)
+            lb_id = amphora.get('loadbalancer_id')
+            # TODO(rm_work): No status change if the amp isn't linked to an LB?
+            if lb_id:
+                v2_utils.wait_for_active(
+                    status_f=(self.app.client_manager.load_balancer.
+                              load_balancer_show),
+                    res_id=lb_id
+                )
 
 
 class FailoverAmphora(command.Command):
@@ -150,12 +167,33 @@ class FailoverAmphora(command.Command):
             metavar='<amphora-id>',
             help='UUID of the amphora.',
         )
+        parser.add_argument(
+            '--wait',
+            action='store_true',
+            help='Wait for action to complete',
+        )
 
         return parser
 
     def take_action(self, parsed_args):
         attrs = v2_utils.get_amphora_attrs(self.app.client_manager,
                                            parsed_args)
-
+        amp_id = attrs.pop('amphora_id')
+        amphora = self.app.client_manager.load_balancer.amphora_show(amp_id)
         self.app.client_manager.load_balancer.amphora_failover(
-            amphora_id=attrs.pop('amphora_id'))
+            amphora_id=amp_id)
+
+        if parsed_args.wait:
+            lb_id = amphora.get('loadbalancer_id')
+            if lb_id:
+                v2_utils.wait_for_active(
+                    status_f=(self.app.client_manager.load_balancer.
+                              load_balancer_show),
+                    res_id=lb_id
+                )
+            else:
+                v2_utils.wait_for_delete(
+                    status_f=(self.app.client_manager.load_balancer.
+                              amphora_show),
+                    res_id=amp_id
+                )

octaviaclient/osc/v2/constants.py
@@ -313,3 +313,5 @@ FLAVORPROFILE_COLUMNS = (
     'name',
     'provider_name',
 )
+
+PROVISIONING_STATUS = 'provisioning_status'

octaviaclient/osc/v2/health_monitor.py
@@ -132,6 +132,11 @@ class CreateHealthMonitor(command.ShowOne):
             default=None,
             help="Disable health monitor."
         )
+        parser.add_argument(
+            '--wait',
+            action='store_true',
+            help='Wait for action to complete',
+        )
 
         return parser
 
@@ -142,6 +147,20 @@ class CreateHealthMonitor(command.ShowOne):
         body = {"healthmonitor": attrs}
         data = self.app.client_manager.load_balancer.health_monitor_create(
             json=body)
+
+        if parsed_args.wait:
+            pool = self.app.client_manager.load_balancer.pool_show(
+                data['healthmonitor']['pools'][0]['id'])
+            v2_utils.wait_for_active(
+                status_f=(self.app.client_manager.load_balancer.
+                          load_balancer_show),
+                res_id=pool['loadbalancers'][0]['id']
+            )
+            data = {
+                'healthmonitor': (
+                    self.app.client_manager.load_balancer.health_monitor_show(
+                        data['healthmonitor']['id']))
+            }
 
         formatters = {'pools': v2_utils.format_list}
 
@@ -162,6 +181,11 @@ class DeleteHealthMonitor(command.Command):
             metavar='<health_monitor>',
             help="Health monitor to delete (name or ID)."
         )
+        parser.add_argument(
+            '--wait',
+            action='store_true',
+            help='Wait for action to complete',
+        )
 
         return parser
 
@@ -173,6 +197,13 @@ class DeleteHealthMonitor(command.Command):
 
         self.app.client_manager.load_balancer.health_monitor_delete(
             health_monitor_id=health_monitor_id)
+
+        if parsed_args.wait:
+            v2_utils.wait_for_delete(
+                status_f=(self.app.client_manager.load_balancer.
+                          health_monitor_show),
+                res_id=health_monitor_id
+            )
 
 
 class ListHealthMonitor(lister.Lister):
@@ -316,6 +347,11 @@ class SetHealthMonitor(command.Command):
             default=None,
             help="Disable health monitor."
         )
+        parser.add_argument(
+            '--wait',
+            action='store_true',
+            help='Wait for action to complete',
+        )
 
         return parser
 
@@ -329,6 +365,13 @@ class SetHealthMonitor(command.Command):
 
         self.app.client_manager.load_balancer.health_monitor_set(
             hm_id, json=body)
+
+        if parsed_args.wait:
+            v2_utils.wait_for_active(
+                status_f=(self.app.client_manager.load_balancer.
+                          health_monitor_show),
+                res_id=hm_id
+            )
 
 
 class UnsetHealthMonitor(command.Command):
@@ -378,6 +421,11 @@ class UnsetHealthMonitor(command.Command):
             action='store_true',
             help="Clear the health monitor URL path."
         )
+        parser.add_argument(
+            '--wait',
+            action='store_true',
+            help='Wait for action to complete',
+        )
         return parser
 
     def take_action(self, parsed_args):
@@ -393,3 +441,10 @@ class UnsetHealthMonitor(command.Command):
 
         self.app.client_manager.load_balancer.health_monitor_set(
             hm_id, json=body)
+
+        if parsed_args.wait:
+            v2_utils.wait_for_active(
+                status_f=(self.app.client_manager.load_balancer.
+                          health_monitor_show),
+                res_id=hm_id
+            )

octaviaclient/osc/v2/l7policy.py
@@ -104,6 +104,11 @@ class CreateL7Policy(command.ShowOne):
             default=None,
             help="Disable l7policy."
         )
+        parser.add_argument(
+            '--wait',
+            action='store_true',
+            help='Wait for action to complete',
+        )
 
         return parser
 
@@ -116,6 +121,20 @@ class CreateL7Policy(command.ShowOne):
 
         data = self.app.client_manager.load_balancer.l7policy_create(
             json=body)
+
+        if parsed_args.wait:
+            listener = self.app.client_manager.load_balancer.listener_show(
+                data['l7policy']['listener_id'])
+            v2_utils.wait_for_active(
+                status_f=(self.app.client_manager.load_balancer.
+                          load_balancer_show),
+                res_id=listener['loadbalancers'][0]['id']
+            )
+            data = {
+                'l7policy': (
+                    self.app.client_manager.load_balancer.l7policy_show(
+                        data['l7policy']['id']))
+            }
 
         formatters = {'rules': v2_utils.format_list}
 
@@ -134,6 +153,11 @@ class DeleteL7Policy(command.Command):
             metavar="<policy>",
             help="l7policy to delete (name or ID)."
         )
+        parser.add_argument(
+            '--wait',
+            action='store_true',
+            help='Wait for action to complete',
+        )
 
         return parser
 
@@ -145,6 +169,13 @@ class DeleteL7Policy(command.Command):
 
         self.app.client_manager.load_balancer.l7policy_delete(
             l7policy_id=l7policy_id)
+
+        if parsed_args.wait:
+            v2_utils.wait_for_delete(
+                status_f=(self.app.client_manager.load_balancer.
+                          l7policy_show),
+                res_id=l7policy_id
+            )
 
 
 class ListL7Policy(lister.Lister):
@@ -278,6 +309,11 @@ class SetL7Policy(command.Command):
             default=None,
             help="Disable l7policy."
         )
+        parser.add_argument(
+            '--wait',
+            action='store_true',
+            help='Wait for action to complete',
+        )
 
         return parser
 
@@ -292,6 +328,13 @@ class SetL7Policy(command.Command):
 
         self.app.client_manager.load_balancer.l7policy_set(
             l7policy_id, json=body)
+
+        if parsed_args.wait:
+            v2_utils.wait_for_active(
+                status_f=(self.app.client_manager.load_balancer.
+                          l7policy_show),
+                res_id=l7policy_id
+            )
 
 
 class UnsetL7Policy(command.Command):
@@ -320,6 +363,11 @@ class UnsetL7Policy(command.Command):
             action='store_true',
             help="Clear the l7policy redirect HTTP code."
         )
+        parser.add_argument(
+            '--wait',
+            action='store_true',
+            help='Wait for action to complete',
+        )
         return parser
 
     def take_action(self, parsed_args):
@@ -335,3 +383,10 @@ class UnsetL7Policy(command.Command):
 
         self.app.client_manager.load_balancer.l7policy_set(
             policy_id, json=body)
+
+        if parsed_args.wait:
+            v2_utils.wait_for_active(
+                status_f=(self.app.client_manager.load_balancer.
+                          l7policy_show),
+                res_id=policy_id
+            )

commit 400a3a73e015e4c72c1a5610eecf237bf290e584

Fix API breakage due to new exceptions module
The commit ae244fcf25188f38c28870238e648e765a2e3adf broke API consumers
by moving the OctaviaClientException to a different module. Ensure it
remains available in its previous location by importing it by name.

Change-Id: I35aa0bf105c3b59abb2ff775af12198bc0436572
Story: #2006970
Task: 37698

octaviaclient/api/v2/octavia.py
@@ -20,6 +20,8 @@ from osc_lib import exceptions as osc_exc
 from octaviaclient.api import constants as const
 from octaviaclient.api import exceptions
 
+OctaviaClientException = exceptions.OctaviaClientException
+
 
 def correct_return_codes(func):
     _status_dict = {400: 'Bad Request', 401: 'Unauthorized',
@@ -46,7 +48,7 @@ def correct_return_codes(func):
             else:
                 raise
 
-            raise exceptions.OctaviaClientException(
+            raise OctaviaClientException(
                 code=code,
                 message=message,
                 request_id=request_id)

octaviaclient/tests/unit/api/test_octavia.py
@@ -155,6 +155,12 @@ SINGLE_FVPF_RESP = {'flavorprofile': {'id': FAKE_FVPF, 'name': 'fvpf1'}}
 SINGLE_FVPF_UPDATE = {'flavorprofile': {'provider_name': 'fake_provider'}}
 
 
+class TestAPI(utils.TestCase):
+    def test_client_exception(self):
+        self.assertIs(octavia.OctaviaClientException,
+                      exceptions.OctaviaClientException)
+
+
 class TestOctaviaClient(utils.TestCase):
 
     def setUp(self):
7ce63497a948c42a5899accd381b82c360eca3ba
Tom Weininger <tweining@redhat.com>
2023-08-28 16:45:36 +0200

    Fix validation of HSTS attributes
    Change-Id: Ie09c10bd94af776a31cf447102ea110c73e239fc

--- octaviaclient/osc/v2/validate.py
+++ octaviaclient/osc/v2/validate.py
@@ -60,8 +60,9 @@ def check_listener_attrs(attrs):
     if 'protocol_port' in attrs:
         _validate_TCP_UDP_SCTP_port_range(attrs['protocol_port'],
                                           'protocol-port')
-    extra_hsts_opts_set = any(
-        key in attrs for key in ('hsts_preload', 'hsts_include_subdomains'))
+
+    extra_hsts_opts_set = attrs.get('hsts_preload') or attrs.get(
+        'hsts_include_subdomains')
     if extra_hsts_opts_set and 'hsts_max_age' not in attrs:
         raise exceptions.InvalidValue(
             "Argument hsts_max_age is required when using hsts_preload or "

b1fd82b9893bf1f8d85673d55667ed7f2843e389
Takashi Kajinami <kajinamit@oss.nttdata.com>
2026-01-06 23:07:42 +0900

    Remove old check for Python 2-3 compatibility
    basestring and xrange no longer exist in Python 3 and any remaining
    usage of these causes "undefined name" error.
    
    Change-Id: I0782c6b521aedaad393ec7175414745c01d76048
    Signed-off-by: Takashi Kajinami <kajinamit@oss.nttdata.com>

--- octaviaclient/tests/unit/test_hacking.py
+++ octaviaclient/tests/unit/test_hacking.py
@@ -87,13 +87,6 @@ class HackingTestCase(base.BaseTestCase):
         self.assertEqual(0, len(list(checks.no_log_warn(
             "LOG.warning()"))))
 
-    def test_no_xrange(self):
-        self.assertEqual(1, len(list(checks.no_xrange(
-            "xrange(45)"))))
-
-        self.assertEqual(0, len(list(checks.no_xrange(
-            "range(45)"))))
-
     def test_no_log_translations(self):
         for log in checks._all_log_levels:
             for hint in checks._all_hints:
@@ -124,13 +117,6 @@ class HackingTestCase(base.BaseTestCase):
         f = checks.check_raised_localized_exceptions
         self.assertLinePasses(f, "raise KeyError('Error text')",
                               'neutron_lib/tests/unit/mytest.py')
-
-    def test_check_no_basestring(self):
-        self.assertEqual(1, len(list(checks.check_no_basestring(
-            "isinstance('foo', basestring)"))))
-
-        self.assertEqual(0, len(list(checks.check_no_basestring(
-            "isinstance('foo', six.string_types)"))))
 
     def test_check_no_eventlet_imports(self):
         f = checks.check_no_eventlet_imports
